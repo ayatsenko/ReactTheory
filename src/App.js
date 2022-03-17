381b21eed953f282985f50229405d2d9e59c27ee
AlexeyYatsenko <al.yacenko@gmail.com>
2022-03-18 01:40:59 +0200

    Block 6

--- src/App.js
+++ src/App.js
@@ -1,118 +1,36 @@
 // eslint-disable-next-line no-use-before-define
 import React, { Component } from 'react'
-import './App.css'
-import Car from './Car/Car'
-import ErrorBoundarys from './ErrorBoundary/ErrorBoundarys'
-import Counter from './Counter/Counter'
-
-// eslint-disable-next-line no-unused-vars
-export const ClickedContext = React.createContext(false)
+import classes from './App.module.scss'
+import { Route, NavLink, Routes } from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
 
 class App extends Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor (props) {
-    // console.log('App constructor')
-    super(props)
-
-    this.state = {
-      clicked: false,
-      cars: [
-        { name: 'Ford', year: 2018 },
-        { name: 'Audi', year: 2016 },
-        { name: 'Mazda', year: 2010 }
-      ],
-      pageTitle: 'React components',
-      showCars: false
-    }
-  }
-
-  onChangeName (name, index) {
-    const car = this.state.cars[index]
-    car.name = name
-    // eslint-disable-next-line no-unused-vars
-    const cars = [...this.state.cars]
-    cars[index] = car
-    this.setState({
-      cars
-    })
-  }
-
-  toggleCarsHandler = () => {
-    this.setState({
-      showCars: !this.state.showCars
-    })
-  }
-
-  deleteHandler (index) {
-    const cars = this.state.cars.concat()
-    cars.splice(index, 1)
-    this.setState({ cars })
-  }
-
-  // eslint-disable-next-line react/no-deprecated
-  componentWillMount () {
-    // console.log('App componentWillMount')
-  }
-
-  componentDidMount () {
-    // console.log('App componentDidMount')
-  }
-
   render () {
-    // console.log('App render')
-
-    const divStyle = {
-      textAlign: 'center'
-    }
-
-    let cars = null
-
-    if (this.state.showCars) {
-      cars = this.state.cars.map((car, index) => {
-        return (
-            <ErrorBoundarys
-                key = {index}
-            >
-                <Car
-                    name = {car.name}
-                    year = {car.year}
-                    index = {index}
-                    onDelete = {this.deleteHandler.bind(this, index)}
-                    onChangeName = { event => this.onChangeName(event.target.value, index)}
-                >
-                </Car>
-            </ErrorBoundarys>
-        )
-      })
-    }
-
     return (
-          // eslint-disable-next-line react/react-in-jsx-scope
-          <div style={divStyle}>
-              {/* <h1 style={{ color: 'blue', fontSize: '50px' }}>{this.state.pageTitle}</h1> */}
-            {/* eslint-disable-next-line react/prop-types */}
-              <h1>{this.props.title}</h1>
+            <div>
+                <nav className={classes.nav}>
+                            <NavLink
+                                to="/"
+                                exact="true"
+                                activeclassname={'wfm-active'}
+                            >Home</NavLink>
+                            <NavLink to="/about" activestyle={{
+                              color: 'blue'
+                            }}>About</NavLink>
+                            <NavLink to={{
+                              pathname: '/cars'
+                            }}>Cars</NavLink>
+                </nav>
 
-            <ClickedContext.Provider value={this.state.clicked}>
-              <Counter />
-            </ClickedContext.Provider>
-              <hr />
-              <button
-                  style={{ marginTop: '20px' }}
-                  onClick={this.toggleCarsHandler}>
-                Toggle cars</button>
-
-            <button onClick={() => this.setState({ clicked: true })}>Change clicked</button>
-
-              {/* eslint-disable-next-line array-callback-return */}
-              <div style={{
-                width: 400,
-                margin: 'auto',
-                paddingTop: '20px'
-              }}>
-                { cars }
-              </div>
-          </div>
+                <hr/>
+                <Routes>
+                    {/* localhost:3000 */}
+                    <Route path="/" exact render={() => <h1>Home Page</h1>} />
+                    <Route path="/about" component={<About />} />
+                    <Route path="/cars" component={<Cars />} />
+                </Routes>
+            </div>
     )
   }
 }
--- src/Hoc/Auxiliary.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-
-const Auxiliary = (props) => {
-  return props.children
-}
-
-export default Auxiliary
